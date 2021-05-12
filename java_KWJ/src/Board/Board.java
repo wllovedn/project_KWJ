410d99d91b365d1af9d20f60d73f0cdd6ff8dc37
wllovedn <wllovedn2@naver.com>
2021-05-12 17:23:18 +0900

    12일차 수업내용

--- java_KWJ/src/Board/Board.java
+++ java_KWJ/src/Board/Board.java
@@ -1,88 +1,126 @@
 package Board;
 
-import java.util.Scanner;
-
 public class Board {
-	/* 다음 메뉴를 가지는 게시글 콘솔 프로그램을 만드세요.
-       1. 게시글 등록
-       2. 게시글 수정
-       3. 게시글 삭제
-       4. 게시글 목록 확인
-	   5. 게시글 상세 확인
-	   6. 프로그램 종료 */
-	 
-	private String A = "";
-	private String B = "";
-	private String C = "";
-	private String D = "";
-	private String E = "";
-	
-    
+	private int num; 
+	private String title = "";
+	private String contents = new String();
+	private String writer = "";
+	private String registerDate = "";
+	private int views;
+	private int like;
+	private String type = "";
+	/* 게시글에 필요한 멤버변수를 선언하세요. 
+	 * - 제목, 내용, 작성자, 등록시간, 조회수, 좋아요수, 번호, 타입 */
+	private String regiserDate;
 	
 	
-
-	
-
-	public Board(String a, String b, String c, String d, String e, int x) {
+public Board(int num, String title, String contents, String writer, String registerDate, String type) {
 		super();
-		
-		A = a;
-		B = b;
-		C = c;
-		D = d;
-		E = e;
+		this.num = num;
+		this.title = title;
+		this.contents = contents;
+		this.writer = writer;
+		this.registerDate = registerDate;
+		this.type = type;
 	}
-		
+	
+	
+	/* 기능    : 게시글 정보 출력하는 메소드
+	 * 매개변수 : 없음
+	 * 리턴타입 : 없음 void
+	 * 메소드명 : print 
+	 */
+	
 	public void print() {
-		Scanner scan = new Scanner (System.in);
-		System.out.println("게시글 등록" + A);
-		System.out.println("게시글 수정" + B);
-		System.out.println("게시글 삭제" + C);
-		System.out.println("게시글 목록 확인" + D);
-		System.out.println("게시글 상세 확인" + E);
-		System.out.println("프로그램 종료" + X);
-		
+		views += 1;
+		System.out.println("번호 : " + num);
+		System.out.println(" 제목 : " + title);
+		System.out.println("작성자 : " + writer);
+		System.out.println("조회수 :" + views);
+		System.out.println("좋아요 : " + like);
+		System.out.println("작성일 : " + registerDate);
+		System.out.println("내용 : " + contents);
 		
 	}
+    /* 기능    : 게시글 정보를 요약해서 출력하는 기능(번호, 제목, 작성자, 작성일, 조회수)
+     * 매개변수 : 없음
+     * 리턴타입 : void
+     * 메소드명 : summaryPrint
+     * 
+     */
+	public void summaryPrint() {
+		System.out.print(num + " ");
+		System.out.print(title + " ");
+		System.out.print(writer + " ");
+		System.out.print(registerDate + " ");
+		System.out.print(views + " ");
+		System.out.println();
+		
+	}
+	/* 기능    : 제목과 내용을 수정하는 메소드
+	 * 매개변수 : 수정할 제목, 내용 = > String title, String contents 
+	 * 리던타입 : void (수정 또는 변경하는 메소드는 거의 void)
+	 * 메소드명 : modify
+	 */
+	public void modify(String title, String contents) {
+		this.title = title;
+		this.contents = contents;
+	}
+	
+	
+	
 	public int getNum() {
 		return num;
 	}
 	public void setNum(int num) {
 		this.num = num;
 	}
-	public String getA() {
-		return A;
+	public String getTitle() {
+		return title;
 	}
-	public void setA(String a) {
-		A = a;
+	public void setTitle(String title) {
+		this.title = title;
 	}
-	public String getB() {
-		return B;
+	public String getContents() {
+		return contents;
 	}
-	public void setB(String b) {
-		B = b;
+	public void setContents(String contents) {
+		this.contents = contents;
 	}
-	public String getC() {
-		return C;
+	public String getWriter() {
+		return writer;
 	}
-	public void setC(String c) {
-		C = c;
+	public void setWriter(String writer) {
+		this.writer = writer;
 	}
-	public String getD() {
-		return D;
+	public String getRegiserDate() {
+		return regiserDate;
 	}
-	public void setD(String d) {
-		D = d;
+	public void setRegiserDate(String regiserDate) {
+		this.regiserDate = regiserDate;
 	}
-	public String getE() {
-		return E;
+	public int getViews() {
+		return views;
 	}
-	public void setE(String e) {
-		E = e;
-			
+	public void setView(int views) {
+		this.views = views;
 	}
-}
-	
+	public int getLike() {
+		return like;
+	}
+	public void setLike(int like) {
+		this.like = like;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
 
-  
+
+
+
+
+}
 	
--- java_KWJ/src/Board/BoardTest.java
+++ java_KWJ/src/Board/BoardTest.java
@@ -1,46 +1,139 @@
 package Board;
 
-import java.util.Scanner;
+import  java.util.Scanner ;
 
-public class BoardTest {
-  private static String ;
+public  class  BoardTest {
 
-public static void main(String[] args) {
-	  /*다음 메뉴를 가지는 게시글 콘솔 프로그램을 만드세요.
-	  1. 게시글 등록
-	  2. 게시글 수정
-	  3. 게시글 삭제
-	  4. 게시글 목록 확인
-	  5. 게시글 상세 확인
-	  6. 프로그램 종료
-	  */
-	  Scanner scan = new Scanner(System.in);
-	  System.out.println("게시판 메뉴를 확인하세요 : ");
-	  
-	  String Board = scan.next();
-	  
-			  
-	  switch(Board) {
-       case "1":
-       	System.out.println("게시글 등록");
-       case "2":
-       	System.out.println("게시글 수정");
-       case "3":
-       	System.out.println("게시글 삭제");
-       case "4":
-       	System.out.println("게시글 목록 확인");
-       case "5":
-        System.out.println("게시글 상세 확인");
-       case "6":
-         System.out.println("종료.");
-        break;
-       for(int a = 1 ; a >= 7 ; a +=1) {
-    	   
-       }
+	public  static  void  main ( String [] args ) {
+		/* 다음 메뉴를 고유 한 게시글 콘솔 프로그램을 만드세요.
+		 * 1. 게시글 등록
+		 * 2. 게시글 수정
+		 * 3. 게시글 삭제
+		 * 4. 게시글 목록 확인
+		 * 5. 게시글 상세 확인
+		 * 6. 프로그램 종료 */
+		Scanner scan =  new  Scanner (System.in);
+		int menu =  0 ;
+		int max = 10;
+		Board []board = new Board[max];
+        String title,writer,registerDate,content, type;
+        int num;
+        int count = 0;//저장된 게시글의 갯수(등록할 때 저장할 번지)
+        int views;
+        Board tmpBoard;
+        
+		do {
+			printMenu();// 메뉴를 출력			
+			menu = scan.nextInt ();// 메뉴를 선택
+			// 선택 메뉴에 따라 기능 안내문 출력
+			switch (menu) {
+			case 1 : // 게시글 정보를 입력(제목, 작성자, 작성일,내용)
+				   System.out.println("게시글 정보를 입력하세요.");
+				   System.out.print("제목 : ");
+				   title = scan.next();
+				   System.out.print("작성자 : ");
+				   writer = scan.next();
+				   System.out.print("작성일 : ");
+				   registerDate = scan.next();
+				   System.out.print("내용 : ");
+				   content = scan.next();
+			     
+				   // 타입은 게시글로 지정, 번호는 배열의 번지를 이용
+				   type = "게시글";
+				   num = count + 1;
+				   //입력은 정보를 이용하여 게시글 생성
+				   tmpBoard = new Board(num, title, content, writer, registerDate, type);
+				   //생성된 게시글을 배열에 저장
+				   board[count] = tmpBoard;
+				   count += 1;
+		           break ;
 		
-		System.out.println("잘못된 메뉴 입니다" + );
-	  }
-  	
-   }
+				//title writer registerDate contents
+				   
+				     
+			case 2 : 
+				System.out.print("수정할 게시글 번호를 입력하세요 : ");
+				num = scan.nextInt();
+				
+				if(num <= count && board[num-1] != null){
+					System.out.println("제목 : ");
+					title = scan.next();
+					System.out.println("내용 : ");
+					content = scan.next();
+					board[num-1].modify(title, content);
+									
+				}else {
+					System.out.println("수정할 게시글이 없습니다.");
+				}
+				// 수정할 게시글 번호 입력
+				
+				// 게시글이 있으면 제목과 내용을 입력
+				
+				// 게시글 수정
+				
+				//게시글이 존재하고 => num <= count
+				//게시글이 삭제되지 않았으면 => board[num-1] != null
+				break ;
+			
+			case 3 :
+				System.out.print("삭제할 게시글 번호를 입력하세요 : ");
+				num = scan.nextInt();
+				if(num <= count) {
+					board[num - 1] = null;
+					
+				}
+				    // 삭제할 게시글 번호를 입력
+				
+				    // 삭제 = > 해당 정보를 null
+		        break ;
+			case 4 : 
+				    //모든 게시글을 확인
+				    //board[num-1].print 조회하고 
+					//번호 제목 작성자 작성일만 출력
+				    //num titel writer registerDate 만 출력
+				for(int i = 0 ; i<count; i+=1) {
+					if(board[i] != null) {
+						board[i].summaryPrint();
+						
+					}
+				}
+				    
+				
+					
+					break ;
+			case 5 : 
+				    // -상세 내용을 확인할 게시글을 선택
+				System.out.println("확인할 게시글의 번호를 입력하세요 : ");
+				num = scan.nextInt();
+				 // -해당 게시글의 내용을 출력
+				if(num <= count && board[num-1] != null) {
+					//게시글을 확인하면 조회수를 1증가
+					// board를 확인하면 views 1증가
+				    // 기존 조회수를 가져오고 기존 조회수에 +1을 해서 업데이트
+					//views = board[num-1].getLike();
+					//board[num-1].setLike(views + 1);
+					board[num-1].print();//게시글 조회
+                    
+				}else {
+					System.out.println("해당 게시글이 없거나 삭제되었습니다.");
+				}
+                break ;
+			case 6 : System.out.println ( " 프로그램 종료 " ); break ;
+			default : System.out.println ( " 잘못된 메뉴입니다. " );
+			}
+		} while (menu != 6 );
+		scan.close();
+	}
+    public static void printMenu() {
+    	System.out.println( " ------- 메뉴 ------- " );
+		System.out.println ( " 1. 게시글 등록 " );
+		System.out.println ( " 2. 게시글 수정 " );
+		System.out.println( " 3. 게시글 삭제 " );
+		System.out.println( " 4. 게시글 목록 확인 " );
+		System.out.println( " 5. 게시글 상세 확인 " );
+		System.out.println( " 6. 프로그램 종료 " );
+		System.out.println( " ----------------- " );
+		System.out.println( " 메뉴를 선택하세요 : " );
+    }
 
 }
