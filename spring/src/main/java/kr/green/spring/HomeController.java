cd7173a35ea3d7948032fa5687535cf594f80158
KWJ <wllovedn2@naver.com>
2021-07-06 09:50:36 +0900

    샘플링테스트 프로젝트 생성 및 인코딩 설정

--- spring/src/main/java/kr/green/spring/HomeController.java
+++ spring/src/main/java/kr/green/spring/HomeController.java
@@ -1,33 +1,43 @@
-package kr.green.spring;
+package kr.green.spring.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import kr.green.spring.service.MemberService;
+import lombok.Data;
 
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+	@Autowired
+    MemberService memberService;
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	public ModelAndView home(ModelAndView mv) {
+		mv.setViewName("home");
+		mv.addObject("name","홍길동");
+		System.out.println(memberService.getMember("abc123"));
+		return mv;
 	}
-	
+	@RequestMapping(value = "/signin", method = RequestMethod.GET)
+	public ModelAndView signinGet(ModelAndView mv,
+			UserVo user,  
+			@RequestParam("hobby") String[] hobby) {
+		mv.setViewName("signin");
+		System.out.println("id : " + user.getId());
+		System.out.println("pw : " + user.getPw());
+		for(String tmp : hobby) {
+			System.out.println("취미: " + tmp);
+		}
+		
+		return mv;
+	}
+}
+@Data
+class UserVo{
+	private String id;
+	private String pw;
 }
